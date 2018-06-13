d99bb8a8aaa150540032e94ad80cb9f2d4fd2c98
ram <ram@Sriram>
2018-06-13 22:01:55 +0530

    commit 2 by dhoni

--- Project/src/com/nt/git/sum.java
+++ Project/src/com/nt/git/sum.java
@@ -2,8 +2,8 @@ package com.nt.git;
 
 public class sum {
 	public static void main(String[] args) {
-		int a=10,b=20;
-		System.out.println(a+b);
+		int a=10,b=20,c=30;
+		System.out.println(a+b+c);
 	}
 
 }
